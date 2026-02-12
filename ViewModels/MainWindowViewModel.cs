992435b750f678c6268d7524057b74daa3876deb
Paul Hofmann <paulmarkushofmann@proton.me>
2026-02-12 23:33:10 +0100

    Implement + Button for adding new todo items

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Avalonia_Demo_Kanban.Models;
 
 namespace Avalonia_Demo_Kanban.ViewModels;
@@ -9,14 +10,22 @@ public partial class MainWindowViewModel : ViewModelBase
     public ObservableCollection<TaskItem> DoingTasks { get; } = new();
     public ObservableCollection<TaskItem> DoneTasks { get; } = new();
 
+    public ICommand AddTodoCommand { get; }
+
     public MainWindowViewModel()
     {
         // Seed example data
         TodoTasks.Add(new TaskItem("Example Task 1"));
         TodoTasks.Add(new TaskItem("Example Task 2"));
-
         DoingTasks.Add(new TaskItem("Working on feature"));
-
         DoneTasks.Add(new TaskItem("Initial setup"));
+
+        // Add command
+        AddTodoCommand = new RelayCommand(AddTodo);
+    }
+
+    private void AddTodo()
+    {
+        TodoTasks.Add(new TaskItem("New Task"));
     }
 }
---
+++ ViewModels/RelayCommand.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Windows.Input;
+
+namespace Avalonia_Demo_Kanban.ViewModels;
+
+public class RelayCommand : ICommand
+{
+    private readonly Action _execute;
+
+    public RelayCommand(Action execute)
+    {
+        _execute = execute;
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter) => true;
+
+    public void Execute(object? parameter)
+    {
+        _execute();
+    }
+}
