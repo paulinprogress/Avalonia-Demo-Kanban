bbc78ee17fbeaed8a531b802726dd41664af0053
Paul Hofmann <paulmarkushofmann@proton.me>
2026-02-12 23:41:16 +0100

    Implement text box for adding new tasks

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
+
 using Avalonia_Demo_Kanban.Models;
 
 namespace Avalonia_Demo_Kanban.ViewModels;
 
-public partial class MainWindowViewModel : ViewModelBase
+public partial class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
 {
     public ObservableCollection<TaskItem> TodoTasks { get; } = new();
     public ObservableCollection<TaskItem> DoingTasks { get; } = new();
     public ObservableCollection<TaskItem> DoneTasks { get; } = new();
+
+    private string _newTodoText = "";
+    public string NewTodoText
+    {
+        get => _newTodoText;
+        set
+        {
+            _newTodoText = value;
+            OnPropertyChanged();
+        }
+    }
 
     public ICommand AddTodoCommand { get; }
 
@@ -26,6 +40,16 @@ public partial class MainWindowViewModel : ViewModelBase
 
     private void AddTodo()
     {
-        TodoTasks.Add(new TaskItem("New Task"));
+        if (string.IsNullOrWhiteSpace(NewTodoText)) return;
+        
+        TodoTasks.Add(new TaskItem(NewTodoText));
+        NewTodoText = "";
+    }
+
+    public new event PropertyChangedEventHandler? PropertyChanged;
+
+    protected new void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
