6982844c0b0d1331ed96ba31248c8c4e4d232b4c
Paul Hofmann <paulmarkushofmann@proton.me>
2026-02-15 11:31:25 +0100

    Refectored reusable column ViewModel

---
+++ ViewModels/KanbanColumnViewModel.cs
@@ -0,0 +1,50 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+using Avalonia_Demo_Kanban.Models;
+
+namespace Avalonia_Demo_Kanban.ViewModels;
+
+public class KanbanColumnViewModel : INotifyPropertyChanged
+{
+    public string Title { get; }
+
+    public ObservableCollection<TaskItem> Tasks { get; } = new();
+
+    private string _newTaskText = "";
+    public string NewTaskText
+    {
+        get => _newTaskText;
+        set
+        {
+            _newTaskText = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ICommand AddTaskCommand { get; }
+
+    public KanbanColumnViewModel(string title)
+    {
+        Title = title;
+        AddTaskCommand = new RelayCommand(AddTask);
+    }
+
+    private void AddTask()
+    {
+        if (string.IsNullOrWhiteSpace(NewTaskText))
+            return;
+
+        Tasks.Add(new TaskItem(NewTaskText));
+        NewTaskText = "";
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,55 +1,15 @@
 ﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows.Input;
-
-using Avalonia_Demo_Kanban.Models;
 
 namespace Avalonia_Demo_Kanban.ViewModels;
 
-public partial class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
+public class MainWindowViewModel
 {
-    public ObservableCollection<TaskItem> TodoTasks { get; } = new();
-    public ObservableCollection<TaskItem> DoingTasks { get; } = new();
-    public ObservableCollection<TaskItem> DoneTasks { get; } = new();
-
-    private string _newTodoText = "";
-    public string NewTodoText
-    {
-        get => _newTodoText;
-        set
-        {
-            _newTodoText = value;
-            OnPropertyChanged();
-        }
-    }
-
-    public ICommand AddTodoCommand { get; }
+    public ObservableCollection<KanbanColumnViewModel> Columns { get; } = new();
 
     public MainWindowViewModel()
     {
-        // Seed example data
-        TodoTasks.Add(new TaskItem("Example Task 1"));
-        TodoTasks.Add(new TaskItem("Example Task 2"));
-        DoingTasks.Add(new TaskItem("Working on feature"));
-        DoneTasks.Add(new TaskItem("Initial setup"));
-
-        // Add command
-        AddTodoCommand = new RelayCommand(AddTodo);
-    }
-
-    private void AddTodo()
-    {
-        if (string.IsNullOrWhiteSpace(NewTodoText)) return;
-        
-        TodoTasks.Add(new TaskItem(NewTodoText));
-        NewTodoText = "";
-    }
-
-    public new event PropertyChangedEventHandler? PropertyChanged;
-
-    protected new void OnPropertyChanged([CallerMemberName] string? name = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        Columns.Add(new KanbanColumnViewModel("TODO"));
+        Columns.Add(new KanbanColumnViewModel("DOING"));
+        Columns.Add(new KanbanColumnViewModel("DONE"));
     }
 }
